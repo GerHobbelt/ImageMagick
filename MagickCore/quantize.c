675044efb1c557e3e429efee5c81559cd4b2cac5
dlwr <yuta25.a.k.a.djbudo.a.k.a.dlwr@gmail.com>
2020-05-08 12:11:33 +0900

    remove bulk color reduction

--- MagickCore/quantize.c
+++ MagickCore/quantize.c
@@ -3483,28 +3483,6 @@ static void ReduceImageColors(const Image *image,CubeInfo *cube_info)
     span;
 
   cube_info->next_threshold=0.0;
-  if (cube_info->colors > cube_info->maximum_colors)
-    {
-      double
-        *quantize_error;
-
-      /*
-        Enable rapid reduction of the number of unique colors.
-      */
-      quantize_error=(double *) AcquireQuantumMemory(cube_info->nodes,
-        sizeof(*quantize_error));
-      if (quantize_error != (double *) NULL)
-        {
-          (void) QuantizeErrorFlatten(cube_info,cube_info->root,0,
-            quantize_error);
-          qsort(quantize_error,cube_info->nodes,sizeof(double),
-            QuantizeErrorCompare);
-          if (cube_info->nodes > (110*(cube_info->maximum_colors+1)/100))
-            cube_info->next_threshold=quantize_error[cube_info->nodes-110*
-              (cube_info->maximum_colors+1)/100];
-          quantize_error=(double *) RelinquishMagickMemory(quantize_error);
-        }
-  }
   for (span=cube_info->colors; cube_info->colors > cube_info->maximum_colors; )
   {
     cube_info->pruning_threshold=cube_info->next_threshold;
